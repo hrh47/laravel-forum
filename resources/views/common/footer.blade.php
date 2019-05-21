9ef958c9e4bcdaec06b921ef30af927f692c809b
Andrew Hsieh <hrh47.83@gmail.com>
2019-05-22 06:42:03 +0800

    Add bootstrap html

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="container" style="margin-top: 100px;">
+  <p class="text-center">Copyright ©2019 Forum
+  </p>
+</footer>
---
+++ resources/views/common/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navber-header">
+      <a class="navbar-brand" href="/">Forum</a>
+    </div>
+
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a href="#">登入</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
