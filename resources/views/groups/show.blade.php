f6011d218fcac951e3768e752ebbf1a432ef2626
Andrew Hsieh <hrh47.83@gmail.com>
2019-05-25 15:03:11 +0800

    Add post button

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 	<div class="col-md-12">
 	  <div>
+	  	<a href="{{ route('groups.posts.create', compact('group')) }}" class="btn btn-primary pull-right">
+	  		Write a Post
+	  	</a>
 	  	@can('update', $group)
 	    	<a href="{{ route('groups.edit', compact('group')) }}" class="btn btn-primary pull-right">Edit</a>
 	    @endcan
