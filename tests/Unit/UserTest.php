26a56053b0fdc9e69d6f2af195705cecbcbf11b2
Andrew Hsieh <hrh47.83@gmail.com>
2019-05-31 06:31:47 +0800

    Add user join/quit group unit test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -29,4 +29,32 @@ class UserTest extends TestCase
    		'Illuminate\Database\Eloquent\Collection', $user->posts
    	);
   }
+
+  /** @test */
+  function a_user_can_join_a_group()
+  {
+    $user = create('App\User');
+    $group = create('App\Group');
+
+    $this->assertTrue(! $user->isMemberOf($group));
+
+    $user->join($group);
+    $user = \App\User::find($user->id);
+    $this->assertTrue($user->isMemberOf($group));
+  }
+
+  /** @test */
+  function a_user_can_quit_a_group()
+  {
+    $user = create('App\User');
+    $group = create('App\Group');
+
+    $user->join($group);
+    $user = \App\User::find($user->id);
+    $this->assertTrue($user->isMemberOf($group));
+
+    $user->quit($group);
+    $user = \App\User::find($user->id);
+    $this->assertTrue(! $user->isMemberOf($group));
+  }
 }
